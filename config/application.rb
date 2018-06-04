f174ccf2ccff689c6f7a0866883ba0cf0e6f2933
Nasonov V <qxuken@gmail.com>
2018-06-04 11:06:07 +0300

    add methods to cors

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,7 @@ module DummyTodo
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :options]
+        resource '*', headers: :any, methods: [:get, :post, :options, :put, :delete]
       end
     end
 
